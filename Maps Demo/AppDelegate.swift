606627f972ad518f0a6d0545279c23ff3c6e734a
Mohammad Shaker <mohammad.shaker.103@gmail.com>
2017-12-17 17:24:21 +0200

    ADD: Map view

--- Maps Demo/AppDelegate.swift
+++ Maps Demo/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyC0jAc7bsJe71r5bVoNKS34xIR8lgYOSo4")
+        GMSPlacesClient.provideAPIKey("AIzaSyC0jAc7bsJe71r5bVoNKS34xIR8lgYOSo4")
+        
         return true
     }
 
